e17fb92af1b11b345c4bd38ab925971eafc8cad0
surnachevaad <Aleksandra.Surnacheva@lukoil.com>
2025-11-16 21:42:54 +0300

    upd

--- script.js
+++ script.js
@@ -72,7 +72,7 @@
                 "directions": width < 760 ? remToPixels(-14) : width < 1440 ? remToPixels(-16) : remToPixels(-22) - 3000,
                 "specialists": width < 760 ? remToPixels(-8) : remToPixels(-12),
                 "technology": width < 760 ? remToPixels(-8) : remToPixels(-7),
-                "results": width < 760 ? remToPixels(-22) : width < 1440 ? remToPixels(-6) : remToPixels(-24),
+                "results": width < 760 ? remToPixels(-22) : width < 1440 ? remToPixels(-6) : remToPixels(-10),
             }
             if (targetElement) {
                 gsap.to(window, {
