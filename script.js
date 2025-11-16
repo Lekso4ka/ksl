c45bb678ed3eb9b2ec55ed252a06bea9fda1b827
surnachevaad <Aleksandra.Surnacheva@lukoil.com>
2025-11-16 22:35:15 +0300

    update h

--- script.js
+++ script.js
@@ -68,10 +68,10 @@
             const targetId = link.getAttribute("href").substring(1);
             const targetElement = document.getElementById(targetId);
             const selfOffsetBlocks = {
-                "directions": width < 760 ? remToPixels(-14) : width < 1440 ? remToPixels(-16) : remToPixels(-22) - 3000,
-                //"specialists": width < 760 ? remToPixels(-8) : remToPixels(-12),
+                //"directions": width < 760 ? remToPixels(-14) : width < 1440 ? remToPixels(-16) : remToPixels(-22) - 3000,
+                "specialists": width < 760 ? remToPixels(-8) : remToPixels(-12),
                 "technology": width < 760 ? remToPixels(-8) : remToPixels(-7),
-                "results": width < 760 ? remToPixels(-22) : width < 1440 ? remToPixels(-6) : remToPixels(-10),
+                //"results": width < 760 ? remToPixels(-22) : width < 1440 ? remToPixels(-6) : remToPixels(-10),
             }
             if (targetElement) {
                 gsap.to(window, {
